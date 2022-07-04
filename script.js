df765741675d228a8240082461c0fafc6608ceae
Adam Baker <adam_baker@live.co.uk>
2022-07-04 12:48:35 +0100

    created a function to fetch data from json file.

--- script.js
+++ script.js
@@ -1,45 +1,88 @@
-const labels = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const main = async () => {
+  const reponse = await fetch("./data.json");
+  const days = await reponse.json();
 
-const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "",
-      backgroundColor: "hsl(10, 79%, 65%)",
-      borderColor: "rgb(255, 99, 132)",
-      data: [17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
-      borderRadius: 4,
-    },
-  ],
-};
+  console.log(days);
 
-const config = {
-  type: "bar",
-  data: data,
-  options: {
-    plugins: {
-      legend: {
-        display: false,
+  const labels = [];
+  days.forEach((day) => {
+    labels.push(day.day);
+  });
+  const daysData = [];
+  days.forEach((day) => {
+    daysData.push(day.amount);
+  });
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "",
+        backgroundColor: [
+          "hsl(10, 79%, 65%)",
+          "hsl(10, 79%, 65%)",
+          "hsl(186, 34%, 60%)",
+          "hsl(10, 79%, 65%)",
+          "hsl(10, 79%, 65%)",
+          "hsl(10, 79%, 65%)",
+          "hsl(10, 79%, 65%)",
+        ],
+        hoverBackgroundColor: [
+          "hsl(10, 79%, 75%)",
+          "hsl(10, 79%, 75%)",
+          "hsl(186, 34%, 75%)",
+          "hsl(10, 79%, 75%)",
+          "hsl(10, 79%, 75%)",
+          "hsl(10, 79%, 75%)",
+          "hsl(10, 79%, 75%)",
+        ],
+        borderColor: "rgb(255, 99, 132)",
+        data: daysData,
+        borderRadius: 4,
       },
-    },
-    scales: {
-      y: {
-        display: false,
-        grid: {
+    ],
+  };
+
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      plugins: {
+        tooltip: {
+          displayColors: false,
+          callbacks: {
+            title: () => {
+              "";
+            },
+            label: (context) => {
+              return "$" + context.formattedValue;
+            },
+          },
+        },
+        legend: {
           display: false,
         },
       },
-      x: {
-        grid: {
-          drawBorder: false,
+      scales: {
+        y: {
+          display: false,
+          grid: {
+            display: false,
+          },
+        },
+        x: {
+          grid: {
+            drawBorder: false,
+            display: false,
+          },
+        },
+        ticks: {
           display: false,
         },
       },
-      ticks: {
-        display: false,
-      },
     },
-  },
-};
+  };
 
-const myChart = new Chart(document.getElementById("myChart"), config);
+  const myChart = new Chart(document.getElementById("myChart"), config);
+};
+main();
